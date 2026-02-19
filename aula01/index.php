9a91ad58b379f4b272b5298b1a8482a1f937756a
Rafael Ramos Araujo <159478325+rafaelramosaraujo@users.noreply.github.com>
2026-02-19 07:36:00 -0300

    Add files via upload

---
+++ aula01/index.php
@@ -0,0 +1,15 @@
+<?php
+    echo "lima mamador";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>boss - TK</title>
+</head>
+<body>
+    
+</body>
+</html>
