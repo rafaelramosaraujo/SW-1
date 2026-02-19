8f5e93096a67393d64b0fe10ad24752a33488815
Rafael Ramos Araujo <159478325+rafaelramosaraujo@users.noreply.github.com>
2026-02-19 08:45:52 -0300

    Add files via upload

---
+++ aula01/exemplo2.php
@@ -0,0 +1,13 @@
+<?php
+   // VERIFICAR CONTEÚDO E TIPOS DE UMA VARIÁVEL
+    $x = 10.5;
+    $x = 5;
+    echo "o valor de X é: $x <br>";
+
+    echo "o valor de X é:". $x;
+    echo "<hr>";
+
+    //COMANDOS DE DEPURAÇÃO DE CÓDIGO
+    var_dump($x);
+    print_r($x);
+?>
---
+++ aula01/exemplo5.php
@@ -0,0 +1,16 @@
+<?php
+     $dia = "segunda";
+     switch ($dia){
+          case "segunda":
+               echo "inicio da semana!";
+               break;
+          case "sexta":
+               echo "Fim de semana chegando!";
+               break;
+          case "sabado":
+               echo "Fim de semana!";
+               break;
+          default:
+               echo "Dia normal.";
+          }
+?>
